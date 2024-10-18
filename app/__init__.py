0b64acef29e1314ce42cc6da2d4c1390f46d628c
NW6617 <NW6617@engie.com>
2024-10-19 00:04:43 +0200

    modfied env variable name for flask secret key

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 
     # Import and register blueprints, models, etc.
     from .routes import main, auth
